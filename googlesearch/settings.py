c094e7812f5b9c98bc2683ca98be02a7fa7900b1
Russ Ferriday <russf@topia.com>
2016-06-09 09:24:23 +0100

    -

--- googlesearch/settings.py
+++ googlesearch/settings.py
@@ -8,5 +8,6 @@ BASE_SOCKS_PORT = os.getenv('BASE_SOCKS_PORT', '9050')
 SOCKS_PORT_COUNT = int(os.getenv('SOCKS_PORT_COUNT', '50'))
 TOR_HOST = os.getenv('TOR_HOST', 'tornet')
 
+base_port = int(BASE_SOCKS_PORT)
 PROXY_LIST = [PROXY_LIST_TEMPLATE.format(host=TOR_HOST, port=p) for p in
-              range(BASE_SOCKS_PORT, BASE_SOCKS_PORT + SOCKS_PORT_COUNT)]
+              range(base_port, base_port + SOCKS_PORT_COUNT)]
